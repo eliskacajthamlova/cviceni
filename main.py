b1d08b69ef26f63fb9ec866467ec5982500bf307
unknown <eliska.cajthamlova@infis.cz>
2025-03-11 09:38:42 +0100

    text

---
+++ main.py
@@ -0,0 +1,26 @@
+#cajthamlova
+#priklad pretypovani na cislo
+cislo = int(input())
+#priklad pretypovani na boolean
+pravdaNepravda = bool(input())
+
+
+#git prikaz 1
+if cislo == 42:
+    #if
+    print(("cislo je kladne"))
+    #elif
+    elif(("pokud je cislo > 0"))
+print(("cislo je zaporne"))
+#else
+else(("musite zadat jine cislo"))
+
+#logicke operatory
+#or
+a=12
+b=18
+print(a > 12 or b < 18)
+print(b > 12 or a < 18)
+print(a < 18 or b > 12)
+
+
